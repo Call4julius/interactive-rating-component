b776e5032141bb8013c4cbfb968fb116f3cf306f
UWUIGBE JULIIUS OSEMWENGIE <call4julius@gmail.com>
2024-01-04 10:12:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,28 @@
 let ratingNumber;
 let ratings = document.querySelector(".ratings");
 let submit = document.querySelector(".button");
+let closeThankYou = document.querySelector(".close-btn");
+let caution = document.querySelector(".caution");
 
 ratings.addEventListener("click", (e) => {
     ratingNumber = e.target.innerText;
+    submit.classList.remove("m-top");
+    caution.classList.add("hidden");
 });
 
 submit.addEventListener("click", () => {
     if (ratingNumber == null) {
-        alert("😒 Kindly rate us by clicking a number and then submit.");
+        submit.classList.add("m-top")
+        caution.classList.remove("hidden");
     } else {
         document.querySelector(".score").innerText = ratingNumber;
         document.querySelector("#rating-state").classList.add("hidden");
         document.querySelector("#thank-you-state").classList.remove("hidden");
     }
+});
+
+closeThankYou.addEventListener("click", () => {
+    document.querySelector("#rating-state").classList.remove("hidden");
+    document.querySelector("#thank-you-state").classList.add("hidden");
+    ratingNumber = null;
 });
